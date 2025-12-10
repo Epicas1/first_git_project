c8ce20b80c89259287dcbdc3ce7d59beb845b3d0
Epicas1 <epifaniocasarez@gmail.com>
2025-12-10 09:16:52 -0500

    Update HelloWorld.java

--- src/day01/HelloWorld.java
+++ src/day01/HelloWorld.java
@@ -7,5 +7,10 @@ public class HelloWorld {
         System.out.println(" Hello World ");
 
         System.out.println(" First Git Project");
+           System.out.println(" Hello Cydeo ");
+           System.out.println(" Hello World ");
+           System.out.println(" HEllo Proggramer");
+           
+        
     }
 }
